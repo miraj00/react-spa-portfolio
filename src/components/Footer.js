cbff8aa27890d74595b9dd26f72e08e860f7e1a7
miraj <miraj00@gmail.com>
2021-10-31 15:28:02 -0400

    Components added

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import React  from 'react';
+
+
+
+function Footer() {
+    return (
+
+        <ul> 
+        <li> <a href="#">848.391.9512</a> </li>
+        <li> <a href="mailto:miraj00@gmail.com" target="_blank">miraj00@gmail.com</a> </li>
+        <li> <a href="https://github.com/miraj00" target="_blank">Github Link</a> </li>
+        <li> <a href="#">Twitter Link</a> </li>
+        <li> <a href="#">Spotify Link</a> </li>
+      </ul>  
+
+        )
+    }   
+
+
+
+
+
+
+
+
+
+
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React  from 'react';
+
+
+function Header() {
+    return (
+
+        <ul> 
+        <li><a href="./index.html#about">About Me</a></li>
+        <li> <a href="./index.html#work">Work</a> </li>
+        <li> <a href="./index.html#contact">Contact Me</a> </li>
+        <li> <a href="./resume.html">Resume</a> </li>
+     </ul>  
+
+        )
+    }   
+
+
+
+
+
+export default Header;
