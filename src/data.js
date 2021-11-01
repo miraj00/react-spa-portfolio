aede1a41afa561bf679e216be5381a37b5c2aff3
miraj <miraj00@gmail.com>
2021-11-01 01:33:15 -0400

    portfolio projects added

---
+++ src/data.js
@@ -0,0 +1,68 @@
+import home from './assets/images/dream-home.png';
+import national_park from './assets/images/national-park.JPG';
+import PizzaHunt from './assets/images/pizza-hunt.png';
+import PhotoPort from './assets/images/photo-port.JPG';
+import zookeepr from './assets/images/zookeeper.JPG';
+import weatherDashboard from './assets/images/weather-dashboard.JPG';
+import timerQuiz from './assets/images/timer-quiz.JPG';
+import noteTakerApp from './assets/images/note-taker-app.png';
+
+
+export default [
+    {
+        "id": 1,
+        "name": "Dream-Home",
+        "image": home,
+        "github": "https://github.com/miraj00/Dream-Home",
+        "deploy": "https://afternoon-thicket-61353.herokuapp.com/"
+    },
+    {
+        "id": 2,
+        "name": "National-Park-Trip-Planner",
+        "image": national_park,
+        "github": "https://github.com/miraj00/National-Park-Trip-Planner",
+        "deploy": "https://miraj00.github.io/National-Park-Trip-Planner/"
+    },
+    {
+        "id": 3,
+        "name": "Pizza-Hunt",
+        "image": PizzaHunt,
+        "github": "https://github.com/miraj00/pizza-hunt",
+        "deploy": "https://pacific-wave-04823.herokuapp.com/"
+    },
+    {
+        "id": 4,
+        "name": "Photo-Port",
+        "image": PhotoPort,
+        "github": "https://github.com/miraj00/photo-port",
+        "deploy": "https://miraj00.github.io/photo-port/"
+    },
+    {
+        "id": 5,
+        "name": "Zookeeper",
+        "image": zookeepr,
+        "github": "https://github.com/miraj00/zookeepr",
+        "deploy": "https://limitless-reef-31179.herokuapp.com/animals"
+    },
+    {
+        "id": 6,
+        "name": "Weather Dashboard",
+        "image": weatherDashboard,
+        "github": "https://github.com/miraj00/weather-dashboard",
+        "deploy": "https://miraj00.github.io/weather-dashboard/"
+    },
+    {
+        "id": 7,
+        "name": "Timer Quiz",
+        "image": timerQuiz,
+        "github": "https://github.com/miraj00/Timer-Quiz",
+        "deploy": "https://miraj00.github.io/Timer-Quiz/"
+    },
+    {
+        "id": 8,
+        "name": "Note Taker App",
+        "image": noteTakerApp,
+        "github": "https://github.com/miraj00/note-taker-app",
+        "deploy": "https://lit-cliffs-71084.herokuapp.com/"
+    }
+]
