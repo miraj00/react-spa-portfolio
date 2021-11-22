c4ca2bfa2996c4fe287b4ef8a26fc2e33d4498f8
miraj <miraj00@gmail.com>
2021-11-21 23:01:20 -0500

    global-trade project added

--- src/data.js
+++ src/data.js
@@ -6,60 +6,67 @@ import zookeepr from './assets/images/zookeeper.JPG';
 import weatherDashboard from './assets/images/weather-dashboard.JPG';
 import timerQuiz from './assets/images/timer-quiz.JPG';
 import noteTakerApp from './assets/images/note-taker-app.png';
-
+import global from './assets/images/global-trade.JPG';
 
 export default [
     {
         "id": 1,
+        "name": "Global-Trade",
+        "image": global,
+        "github": "https://github.com/miraj00/global-trade",
+        "deploy": "https://still-eyrie-57534.herokuapp.com/"
+    },
+    {
+        "id": 2,
         "name": "Dream-Home",
         "image": home,
         "github": "https://github.com/miraj00/Dream-Home",
         "deploy": "https://afternoon-thicket-61353.herokuapp.com/"
     },
     {
-        "id": 2,
+        "id": 3,
         "name": "National-Park-Trip-Planner",
         "image": national_park,
         "github": "https://github.com/miraj00/National-Park-Trip-Planner",
         "deploy": "https://miraj00.github.io/National-Park-Trip-Planner/"
     },
     {
-        "id": 3,
+        "id": 4,
         "name": "Pizza-Hunt",
         "image": PizzaHunt,
         "github": "https://github.com/miraj00/pizza-hunt",
         "deploy": "https://pacific-wave-04823.herokuapp.com/"
     },
     {
-        "id": 4,
+        "id": 5,
         "name": "Photo-Port",
         "image": PhotoPort,
         "github": "https://github.com/miraj00/photo-port",
         "deploy": "https://miraj00.github.io/photo-port/"
     },
     {
-        "id": 5,
+        "id": 6,
         "name": "Zookeeper",
         "image": zookeepr,
         "github": "https://github.com/miraj00/zookeepr",
         "deploy": "https://limitless-reef-31179.herokuapp.com/animals"
     },
     {
-        "id": 6,
+        "id": 7,
         "name": "Weather Dashboard",
         "image": weatherDashboard,
         "github": "https://github.com/miraj00/weather-dashboard",
         "deploy": "https://miraj00.github.io/weather-dashboard/"
     },
     {
-        "id": 7,
+        "id": 8,
         "name": "Timer Quiz",
         "image": timerQuiz,
         "github": "https://github.com/miraj00/Timer-Quiz",
         "deploy": "https://miraj00.github.io/Timer-Quiz/"
     },
     {
-        "id": 8,
+        "id": 9,
         "name": "Note Taker App",
         "image": noteTakerApp,
         "github": "https://github.com/miraj00/note-taker-app",
